018008fb378d14f0e9d0ea78a9b0641ece4ed9e4
lakshmansai1221 <50866643+lakshmansai1221@users.noreply.github.com>
2021-01-13 20:13:55 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,8 @@
+function addNumber(a , b)
+ {
+ 
+ var total = a+b;
+ return total;
+ }
+ var output = addNumber(4,2); 
+ document.write("<h1>The total is : "+output+"</h1>");
---
+++ while_loop.js
@@ -0,0 +1,9 @@
+var x = 1; 
+     
+
+while (x <= 4) 
+{ 
+    document.write("Value of x:" + x + "<br />"); 
+
+    x++; 
+} 
